052291ff3f5b1a2c82338511d17dfd736b422f2b
An980 <9807594@mail.ru>
2022-09-15 19:58:53 +0300

    test succes, final

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -11,14 +11,14 @@ class postmanEchoTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("new data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("failed data"))
+                .body("data", equalTo("new data"))
         ;
     }
 }
